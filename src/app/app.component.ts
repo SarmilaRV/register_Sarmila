71853a6294eb2941a0e08db6f27d9dce5ad49faf
Sarmila R V <sivasarmi2001@gmail.com>
2024-03-12 18:20:04 +0530

    implement the validation

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,10 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'register_form';
+
+  formdata:any = {};
+
+  submit(){
+    console.log(this.formdata);
+  }
 }
